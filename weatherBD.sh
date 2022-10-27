07540d6331258cf0d25a2678ab1f659676267579
Zaber Al Rafian <36133897+rafian-git@users.noreply.github.com>
2022-10-27 17:19:30 +0600

    Update weatherBD.sh

--- weatherBD.sh
+++ weatherBD.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 declare -a arr=("dhaka" "chittagong" "barishal" "rangpur" "mymensing" "sylhet" "khulna" "rajshahi")
+found=false
 for i in "${arr[@]}"
 do
  if [ -z $DIVISION ] || [ "$DIVISION" = "$i" ] || [ "$DIVISION" = "" ]; then
   weather=$(curl -s http://wttr.in/$i?format=2)
   echo "$i :    $weather"
+  found=true
  fi
 done
+if [ "$found" = false ]; then
+ echo -e '\033[31m wrong division provided'
+fi
